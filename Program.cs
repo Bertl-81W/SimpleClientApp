c88f358bc2e0eddc00fb4953a365fc45382ea73a
Bertl-81W <78581189+Bertl-81W@users.noreply.github.com>
2025-12-29 23:37:51 -0700

    add simple client list with an input

--- Program.cs
+++ Program.cs
@@ -1,24 +1,23 @@
-﻿using System;
+﻿using System.Collections.Generic;
 
 List<string> clients = new List<string>();
 
 while (true)
 {
-    Console.WriteLine("Enter client name to add (or type 'exit' to quit):");
-    string input = Console.ReadLine()!;
+    Console.WriteLine("Enter client name (or type 'exit' to finish):");
+    string input = Console.ReadLine();
 
-    if (input.ToLower() == "exit")
+    if (input == "exit")
     {
         break;
     }
-
-    if (!string.IsNullOrWhiteSpace(input))
-    {
         clients.Add(input);
-        Console.WriteLine($"Client '{input}' added.");
+        Console.WriteLine($"Client added!\n");
     }
-    else
-    {
-        Console.WriteLine("Client name cannot be empty.");
-    }
-}
+
+        Console.WriteLine("All clients:"); 
+        foreach (string client in clients)
+        {
+            Console.WriteLine(client);
+        }
+    
